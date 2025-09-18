1d372cd6f9d5acf95f82605eb51bb41335673761
Artur Sousa <126983187+xavierrreu@users.noreply.github.com>
2025-09-18 22:56:26 +0000

    feat: add subscriber

--- eventos/src/main/java/br/univille/Publisher.java
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -1,14 +1,32 @@
 package br.univille;
 
+import com.azure.core.amqp.AmqpTransportType;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 public class Publisher {
     public static void main(String[] args) {
-        var topicName="topic-das1-b";
-        var servidor="https://sbdas12025a.servicebus.windows.net";
+        var topicName = "topic-das1-b";
+        var servidor = "sbdas12025a.servicebus.windows.net";
 
-        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder().build();
+
+        String chave = System.getenv("CHAVE");
+        
+        ServiceBusSenderClient senderClient =
+            new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace(servidor)
+                .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+                .connectionString(chave)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+        
+        senderClient.sendMessage(
+            new ServiceBusMessage(
+                "mensagem unica"));
         
     }
 }
